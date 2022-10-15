60a41b6f6c5480d9f180bb7c26394b56478ebc29
théo bouverat <theo2@DESKTOP-V613FJN.localdomain>
2022-10-16 00:50:46 +0200

    das

--- main.c
+++ main.c
@@ -14,14 +14,5 @@
 
 int	main(void)
 {
-	char str[] = "bonjl";
-	char **ptr;
-	int	index = 0;
-	ptr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i');
-	index = 0;
-	while (ptr[index] != 0)
-	{
-		printf ("%s\n", ptr[index++]);
-	}
 	return (0);
 }
