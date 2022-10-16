87d08babb08f4e3301db745610230b02c4472499
théo bouverat <theo2@DESKTOP-V613FJN.localdomain>
2022-10-16 17:41:09 +0200

    enorme

---
+++ ft_striteri.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbouvera <tbouvera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 16:30:31 by tbouvera          #+#    #+#             */
+/*   Updated: 2022/10/12 10:24:43 by tbouvera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	index;
+
+	index = 0;
+	while (s[index] != '\0')
+	{
+		f(index, &s[index]);
+		index ++;
+	}
+}
