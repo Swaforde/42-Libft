77709512f160db7133f40b217ac0448faf15853e
théo bouverat <theo2@DESKTOP-V613FJN.localdomain>
2022-10-16 21:20:00 +0200

    fixe

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,6 +6,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	index;
 
 	index = 0;
+	if (!src && !dest)
+		return (NULL);
 	while (index < n && n != 0)
 	{
 		((unsigned char *)dest)[index] = ((unsigned char *)src)[index];
