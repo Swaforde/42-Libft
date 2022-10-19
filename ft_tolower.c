ebf9322e361d4569b01bf5f9a5d0b63f67c17c4e
Theo Bouverat <tbouvera@student.42lausanne.ch>
2022-10-19 10:20:59 +0200

    update

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,18 +6,15 @@
 /*   By: tbouvera <tbouvera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 11:55:26 by tbouvera          #+#    #+#             */
-/*   Updated: 2022/10/17 11:55:28 by tbouvera         ###   ########.fr       */
+/*   Updated: 2022/10/19 09:45:47 by tbouvera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdio.h"
 #include "libft.h"
 
 int	ft_tolower(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		c += 32;
-	if (c == EOF)
-		return (-1);
-	return ((unsigned char)c);
+	return (c);
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -6,18 +6,15 @@
 /*   By: tbouvera <tbouvera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 11:55:30 by tbouvera          #+#    #+#             */
-/*   Updated: 2022/10/18 10:42:17 by tbouvera         ###   ########.fr       */
+/*   Updated: 2022/10/19 09:45:54 by tbouvera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"
 
 int	ft_toupper(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		c -= 32;
-	if (c == EOF)
-		return (-1);
-	return ((unsigned char)c);
+	return (c);
 }
